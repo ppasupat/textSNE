57fa517eaef33da00d20a4d6caab2fa3d6d00171
Panupong Pasupat <ppasupat@stanford.edu>
2014-03-05 23:34:39 -0800

    Edited the main script

--- test.py
+++ test.py
@@ -9,11 +9,8 @@ for l in o:
     x.append([float(f) for f in toks[1:]])
 x = numpy.array(x)
 
-#from tsne import tsne
-from calc_tsne import tsne
-#out = tsne(x, no_dims=2, perplexity=30, initial_dims=30, USE_PCA=False)
-#out = tsne(x, no_dims=2, perplexity=30, initial_dims=30, use_pca=False)
+from lib.calc_tsne import tsne
 out = tsne(x, no_dims=2, perplexity=30, initial_dims=30)
 
-import render
+from lib import render
 render.render([(title, point[0], point[1]) for title, point in zip(titles, out)], "test-output.rendered.png", width=3000, height=1800)
