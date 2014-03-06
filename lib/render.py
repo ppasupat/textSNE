869ca7949b5c8ce70c7d3942a2ad063579f4139d
Panupong Pasupat <ppasupat@stanford.edu>
2014-03-06 01:18:13 -0800

    Rewire the render.py codes

--- lib/render.py
+++ lib/render.py
@@ -3,16 +3,15 @@ Render t-SNE text labels.
 Requires PIL (Python Imaging Library) and ImageMagick "convert" command.
 """
 
-import sys
-import Image, ImageFont, ImageDraw, ImageChops, string
+import os, sys, string
+from PIL import Image, ImageFont, ImageDraw, ImageChops
 
-import os.path
-#DEFAULT_FONT=os.path.join(os.path.expanduser('~'), "fonts/Vera.ttf")
 DEFAULT_FONT=None
 
 import tempfile
 
-def render(points, filename, width=3000, height=1800, fontfile=DEFAULT_FONT, fontsize=12, margin=0.05, transparency=0.5):
+def render(points, filename, width=3000, height=1800, fontfile=DEFAULT_FONT,
+           fontsize=12, margin=0.05, transparency=0.4):
     """
     Render t-SNE text points to an image file.
     points is a list of tuples of the form (title, x, y).
@@ -24,23 +23,13 @@ def render(points, filename, width=3000, height=1800, fontfile=DEFAULT_FONT, fon
     W = width
     H = height
 
-    #im = Image.new("L", (W, H), 255)
-    im = Image.new("RGBA", (W, H), (0,0,0))
-
-    # use a bitmap font
-    #font = ImageFont.load("/usr/share/fonts/liberation/LiberationSans-Italic.ttf")
+    im = Image.new("L", (W, H), 255)
 
     if fontfile is not None:
         assert os.path.exists(fontfile)
         font = ImageFont.truetype(fontfile, fontsize)
     
-    #draw = ImageDraw.Draw(im)
-    #draw.text((10, 10), "hello", font=font)
-    
-    minx = 0
-    maxx = 0
-    miny = 0
-    maxy = 0
+    minx, maxx, miny, maxy = 0, 0, 0, 0
     for (title, x, y) in points:
         if minx > x: minx = x
         if maxx < x: maxx = x
@@ -56,56 +45,30 @@ def render(points, filename, width=3000, height=1800, fontfile=DEFAULT_FONT, fon
     maxx += dx * margin
     maxy += dy * margin
 
-
     alpha = Image.new("L", im.size, "black")
 
     for (idx, pt) in enumerate(points):
         (title, x, y) = pt
-    #    print x, minx
-    #    print 1. * (x - minx) / (maxx - minx)
-    #    print y, miny
-    #    print 1. * (y - miny) / (maxy - miny)
         x = 1. * (x - minx) / (maxx - minx) * W
         y = 1. * (y - miny) / (maxy - miny) * H
-    #    draw.text((x, y), w, fill=255, font=font)
 
-    # Make a grayscale image of the font, white on black.
+        # Make a grayscale image of the font, white on black.
         pos = (x, y)
         imtext = Image.new("L", im.size, 0)
         drtext = ImageDraw.Draw(imtext)
-        print >> sys.stderr, "Rendering title (#%d):" % idx, repr(title)
+        if idx % 10 == 0:
+            print >> sys.stderr, "\033[F\033[KRendering title (#%d): %s" % \
+                (idx, repr(title))
         if fontfile is not None:
             drtext.text(pos, title, font=font, fill=(256-256*transparency))
         else:
             drtext.text(pos, title, fill=(256-256*transparency))
-#        drtext.text(pos, title, font=font, fill=128)
 
-    # Add the white text to our collected alpha channel. Gray pixels around
-    # the edge of the text will eventually become partially transparent
-    # pixels in the alpha channel.
-    #    alpha = ImageChops.lighter(alpha, imtext)
+        # Add the white text to our collected alpha channel. Gray pixels around
+        # the edge of the text will eventually become partially transparent
+        # pixels in the alpha channel.
         alpha = ImageChops.add(alpha, imtext)
-            
-    # Make a solid color, and add it to the color layer on every pixel
-    # that has even a little bit of alpha showing.
-    #    solidcolor = Image.new("RGBA", im.size, "#ffffff")
-    #    immask = Image.eval(imtext, lambda p: 120 * (int(p != 0)))
-    #    im = Image.composite(solidcolor, im, immask)
-    #    draw.text((x, y), w, fill=0, font=font)
     
-        print >> sys.stderr, "Rendered word #%d" % idx
-    #    if idx % 100 == 99:
-    #        break
-    
-    # Add the alpha channel to the image, and save it out.
-    im.putalpha(alpha)
-
-    tmpf = tempfile.NamedTemporaryFile(suffix=".png")
-
-    #im.save("transtext.png", "PNG")
-    print >> sys.stderr, "Rendering alpha image to file", tmpf.name
-    im.save(tmpf.name)
-
-    cmd = "convert %s -background white -flatten %s" % (tmpf.name, filename)
-    print >> sys.stderr, "Flattening image", tmpf.name, "to", filename, "using command:", cmd
-    os.system(cmd)
+    im.paste(Image.new('L', im.size, 0), mask=alpha)
+    print >> sys.stderr, "\033[F\033[KRendering image to file", filename
+    im.save(filename)
---
+++ run.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os, re, argparse, json
+from codecs import open
+from collections import defaultdict
+
+import numpy as np
+from lib import calc_tsne, render
+
+def read_combined(fin, limit=0):
+    titles, x = [], []
+    num_words, num_dims = [float(f) for f in fin.readline().split()]
+    for i, line in enumerate(fin):
+        if line.strip():
+            toks = line.strip().split()
+            titles.append(toks[0])
+            x.append([float(f) for f in toks[1:]])
+        if i >= limit:
+            break
+    assert len(titles) == len(x)
+    assert all(len(row) == num_dims for row in x)
+    x = np.array(x)
+    return titles, x
+
+def read_separated(fwords, fWe, limit=0):
+    titles, x = [], []
+    for i, line in enumerate(fwords):
+        if line.strip():
+            titles.append(line.strip())
+        if i >= limit:
+            break
+    for i, line in enumerate(fWe):
+        if line.strip():
+            x.append([float(f) for f in line.strip().split()])
+        if i >= limit:
+            break
+    assert len(titles) == len(x)
+    assert all(len(row) == len(x[0]) for row in x)
+    x = np.array(x)
+    return titles, x
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputs', nargs='+',
+                        help='either [embedding] or [input.words] [input.We]')
+    parser.add_argument('output',
+                        help='output filename [outfile.png]')
+    parser.add_argument('-l', '--limit', metavar='L', type=int,
+                        help='limit to the first L words', default=500)
+    args = parser.parse_args()
+    if len(args.inputs) == 1:
+        i_emb = args.inputs[0]
+        with open(i_emb, 'rb') as fin:
+            print 'Reading combined data from %s ...' % i_emb
+            titles, x = read_combined(fin, limit=args.limit)
+    elif len(args.inputs) == 2:
+        i_words, i_We = args.inputs
+        with open(i_words, 'rb') as fwords:
+            print 'Reading vocab from %s ...' % i_words
+            with open(i_We, 'rb') as fWe:
+                print 'Reading vectors from %s ...' % i_We
+                titles, x = read_separated(fwords, fWe, limit=args.limit)
+    else:
+        parser.print_usage()
+        exit(1)
+    
+    out = calc_tsne.tsne(x, no_dims=2, perplexity=30, initial_dims=30)
+    data = [(title, point[0], point[1]) for (title, point) in zip(titles, out)]
+    render.render(data, args.output)
+
+if __name__ == '__main__':
+    main()
