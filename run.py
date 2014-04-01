dac4808d25afa04ea978b8567a5b45a8aa882105
Panupong Pasupat <ppasupat@stanford.edu>
2014-03-31 22:20:43 -0700

    Can randomize words

--- run.py
+++ run.py
@@ -1,44 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, os, re, argparse, json
+import sys, os, re, argparse, json, random
 from codecs import open
 from collections import defaultdict
 
 import numpy as np
 from lib import calc_tsne, render
 
-def read_combined(fin, limit=0, highlights=None):
-    titles, x = [], []
-    remaining_highlights = set(highlights)
-    num_words, num_dims = [float(f) for f in fin.readline().split()]
-    for i, line in enumerate(fin):
-        if i >= limit and not remaining_highlights:
-            break
-        if line.strip():
+def combined_reader(fin):
+    def reader():
+        print fin.readline().split()
+        for line in fin:
             toks = line.strip().split()
-            titles.append(toks[0])
-            x.append([float(f) for f in toks[1:]])
-            remaining_highlights.discard(toks[0])
-    assert len(titles) == len(x)
-    assert all(len(row) == num_dims for row in x)
-    x = np.array(x)
-    return titles, x
+            if toks:
+                yield (toks[0], [float(f) for f in toks[1:]])
+    return reader
 
-def read_separated(fwords, fWe, limit=0, highlights=None):
+def separated_reader(fWe, fwords):
+    def reader():
+        while True:
+            word = fwords.readline().strip()
+            vec = fWe.readline().strip()
+            if not word or not vec:
+                break
+            yield (word, [float(f) for f in vec.split()])
+    return reader
+
+def read_data(reader, limit=0, highlights=None, random_threshold=0.0):
     titles, x = [], []
     remaining_highlights = set(highlights)
-    i = 0
-    while True:
-        word = fwords.readline().strip()
-        vec = fWe.readline().strip()
-        if not word or not vec or (i >= limit and not remaining_highlights):
+    non_highlight_count = 0
+    for i, (word, vec) in enumerate(reader()):
+        if non_highlight_count >= limit and not remaining_highlights:
             break
-        if i < limit or word in remaining_highlights:
+        if word in remaining_highlights:
             titles.append(word)
-            x.append([float(f) for f in vec.split()])
+            x.append(vec)
             remaining_highlights.discard(word)
-        i += 1
+        elif non_highlight_count < limit and random.random() > random_threshold:
+            titles.append(word)
+            x.append(vec)
+            non_highlight_count += 1
     assert len(titles) == len(x)
     assert all(len(row) == len(x[0]) for row in x)
     x = np.array(x)
@@ -51,6 +54,9 @@ def main():
     parser.add_argument('outdir', help='output directory')
     parser.add_argument('-l', '--limit', metavar='L', type=int,
                         help='limit to the first L words', default=500)
+    parser.add_argument('-r', '--random-threshold', type=float, default=0.0,
+                        help='randomly ignore words with this probability'
+                        'when finding top L words')
     parser.add_argument('-n', '--normalize', action='store_true',
                         help='normalize each word vector to unit L2 norm')
     parser.add_argument('-p', '--pca', type=int, default=0,
@@ -78,16 +84,20 @@ def main():
         i_emb = args.inputs[0]
         with open(i_emb, 'rb') as fin:
             print 'Reading combined data from %s ...' % i_emb
-            titles, x = read_combined(fin, limit=args.limit,
-                                      highlights=all_highlight_words)
+            titles, x = read_data(combined_reader(fin),
+                                  limit=args.limit,
+                                  highlights=all_highlight_words,
+                                  random_threshold=args.random_threshold)
     elif len(args.inputs) == 2:
         i_We, i_words = args.inputs
         with open(i_words, 'rb') as fwords:
             print 'Reading vocab from %s ...' % i_words
             with open(i_We, 'rb') as fWe:
                 print 'Reading vectors from %s ...' % i_We
-                titles, x = read_separated(fwords, fWe, limit=args.limit,
-                                           highlights=all_highlight_words)
+                titles, x = read_data(separated_reader(fWe, fwords),
+                                      limit=args.limit,
+                                      highlights=all_highlight_words,
+                                      random_threshold=args.random_threshold)
     else:
         parser.print_usage()
         exit(1)
